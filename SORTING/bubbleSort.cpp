0241d20a2a0ca21104e6499103f68ac0291e1094
diwakarshukla941 <shukladiwakar941@gmail.com>
2025-04-11 23:36:51 +0530

    sorting algorithms

---
+++ SORTING/bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    // Traverse through all elements
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if current element is greater than the next
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
